9e344c5ece1f7861c05319e8bb0e63c9f5fedeb0
danwatt1987 <44509557+danwatt1987@users.noreply.github.com>
2021-05-11 10:59:57 +0100

    Add files via upload
    change to pie, set height of .appto 100vh

--- src/Chart.js
+++ src/Chart.js
@@ -1,71 +1,122 @@
 import React from 'react'
-import { ResponsiveSwarmPlot } from '@nivo/swarmplot'
-import {data} from './data'
+import { ResponsivePie } from '@nivo/pie'
+import data from './data.json'
   
+console.log(data)
+
 function Chart() {
+
     return (
-     
-            <ResponsiveSwarmPlot
+        <ResponsivePie
         data={data}
-        groups={[ 'group A', 'group B', 'group C' ]}
-        value="price"
-        valueFormat="$.2f"
-        valueScale={{ type: 'linear', min: 0, max: 500, reverse: false }}
-        size={{ key: 'volume', values: [ 4, 20 ], sizes: [ 6, 20 ] }}
-        forceStrength={4}
-        simulationIterations={100}
-        borderColor={{
-            from: 'color',
-            modifiers: [
-                [
-                    'darker',
-                    0.6
-                ],
-                [
-                    'opacity',
-                    0.5
+        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        innerRadius={0.5}
+        padAngle={0.7}
+        cornerRadius={3}
+        activeOuterRadiusOffset={8}
+        borderWidth={1}
+        borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
+        arcLinkLabelsSkipAngle={10}
+        arcLinkLabelsTextColor="#333333"
+        arcLinkLabelsThickness={2}
+        arcLinkLabelsColor={{ from: 'color' }}
+        arcLabelsSkipAngle={10}
+        arcLabelsTextColor={{ from: 'color', modifiers: [ [ 'darker', 2 ] ] }}
+        defs={[
+            {
+                id: 'dots',
+                type: 'patternDots',
+                background: 'inherit',
+                color: 'rgba(255, 255, 255, 0.3)',
+                size: 4,
+                padding: 1,
+                stagger: true
+            },
+            {
+                id: 'lines',
+                type: 'patternLines',
+                background: 'inherit',
+                color: 'rgba(255, 255, 255, 0.3)',
+                rotation: -45,
+                lineWidth: 6,
+                spacing: 10
+            }
+        ]}
+        fill={[
+            {
+                match: {
+                    id: 'ruby'
+                },
+                id: 'dots'
+            },
+            {
+                match: {
+                    id: 'c'
+                },
+                id: 'dots'
+            },
+            {
+                match: {
+                    id: 'go'
+                },
+                id: 'dots'
+            },
+            {
+                match: {
+                    id: 'python'
+                },
+                id: 'dots'
+            },
+            {
+                match: {
+                    id: 'scala'
+                },
+                id: 'lines'
+            },
+            {
+                match: {
+                    id: 'lisp'
+                },
+                id: 'lines'
+            },
+            {
+                match: {
+                    id: 'elixir'
+                },
+                id: 'lines'
+            },
+            {
+                match: {
+                    id: 'javascript'
+                },
+                id: 'lines'
+            }
+        ]}
+        legends={[
+            {
+                anchor: 'bottom',
+                direction: 'row',
+                justify: false,
+                translateX: 0,
+                translateY: 56,
+                itemsSpacing: 0,
+                itemWidth: 100,
+                itemHeight: 18,
+                itemTextColor: '#999',
+                itemDirection: 'left-to-right',
+                itemOpacity: 1,
+                symbolSize: 18,
+                symbolShape: 'circle',
+                effects: [
+                    {
+                        on: 'hover',
+                        style: {
+                            itemTextColor: '#000'
+                        }
+                    }
                 ]
-            ]
-        }}
-        margin={{ top: 80, right: 100, bottom: 80, left: 100 }}
-        axisTop={{
-            orient: 'top',
-            tickSize: 10,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'group if vertical, price if horizontal',
-            legendPosition: 'middle',
-            legendOffset: -46
-        }}
-        axisRight={{
-            orient: 'right',
-            tickSize: 10,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'price if vertical, group if horizontal',
-            legendPosition: 'middle',
-            legendOffset: 76
-        }}
-        axisBottom={{
-            orient: 'bottom',
-            tickSize: 10,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'group if vertical, price if horizontal',
-            legendPosition: 'middle',
-            legendOffset: 46
-        }}
-        axisLeft={{
-            orient: 'left',
-            tickSize: 10,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'price if vertical, group if horizontal',
-            legendPosition: 'middle',
-            legendOffset: -76
-        }}
-        motionStiffness={50}
-        motionDamping={10}
+            }
+        ]}
     />
       
     )
